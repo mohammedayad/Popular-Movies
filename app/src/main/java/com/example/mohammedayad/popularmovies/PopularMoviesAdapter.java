2604d49d63baf2ce0044feff071c292125dd457d
mohammed.ayad <mohammedayad611@gmail.com>
2017-02-28 00:18:03 +0200

    project architecture commit

---
+++ app/src/main/java/com/example/mohammedayad/popularmovies/PopularMoviesAdapter.java
@@ -0,0 +1,8 @@
+package com.example.mohammedayad.popularmovies;
+
+/**
+ * Created by mohammed.ayad on 2/28/2017.
+ */
+
+public class PopularMoviesAdapter {
+}
---
+++ app/src/main/java/com/example/mohammedayad/popularmovies/utilities/NetworkUtils.java
@@ -0,0 +1,9 @@
+package com.example.mohammedayad.popularmovies.utilities;
+
+/**
+ * Created by mohammed.ayad on 2/27/2017.
+ */
+
+public final class NetworkUtils {
+
+}
